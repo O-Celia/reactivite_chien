7ad6e9ff2803dbeafc3a485092dbc5a9d6f046d7
CeliaOuedraogo <108000888+O-Celia@users.noreply.github.com>
2025-07-15 18:25:11 +0200

    Create user.py

---
+++ email_password/schemas/user.py
@@ -0,0 +1,37 @@
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+
+
+class UserBase(BaseModel):
+    username: str
+    email: str | None = None
+
+
+class UserCreate(UserBase):
+    password: str
+    email: EmailStr
+
+
+class UserUpdate(BaseModel):
+    username: str | None = None
+    email: str
+    password: str | None = None
+
+
+class UserRead(UserBase):
+    id: int
+    created_at: datetime
+    first_login: bool
+
+    class Config:
+        from_attributes = True
+
+
+class LoginData(BaseModel):
+    username: str
+    password: str | None = None
+
+
+class TokenResponse(BaseModel):
+    access_token: str
+    token_type: str
